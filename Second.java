72648b570a9155cf97bba02d7dbc3e5dae7c1aaf
AbrahamLzk <948812511@qq.com>
2018-12-16 00:06:05 +0800

    First add test

---
+++ Second.java
@@ -0,0 +1,6 @@
+public class Second {
+    public static void main(String[]args){
+        String name = "LiuZekun";
+        System.out.println("姓名:"+name);
+    }
+}
---
+++ vstest.java
@@ -0,0 +1,5 @@
+public class vstest{
+	public static void main(String args[]){
+		System.out.println("Hello Linux!!");
+	}
+}
